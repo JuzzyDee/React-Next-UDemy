55eedceb7dfa9c93754160056a96aa7569b63e3c
Justin Davis <justin.davis@energyq.com.au>
2024-08-19 23:25:39 +1000

    Initial Commit
    Shows iFrame error when visiting URL http://localhost:3000/meals/not-real in Chrome Version 127.0.6533.100 (Official Build) (arm64)

---
+++ 05-onwards-foodies-starting-project/app/meals/[mealSlug]/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className='not-found'>
+      <h1>Meal Not found</h1>
+      <p>Unfortunately we couldn&apos;t find that meal.</p>
+    </main>
+  );
+}
